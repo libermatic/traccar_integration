ef573bafe26aa7ca48c03c4480a31a7ac6b6480d
Sun Howwrongbum <sun@libermatic.com>
2024-03-25 15:38:45 +0000

    feat: add TraccarSettings

---
+++ traccar_integration/traccar/doctype/traccar_settings/traccar_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Libermatic and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Traccar Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
